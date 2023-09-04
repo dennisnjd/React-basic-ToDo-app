30fc92350effa8ace80ee6927077fc326e9841ba
dennisnjd <dennisnjd@gmail.com>
2023-09-04 16:41:00 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [toDos, setTodos] = useState([]);
+  const [toDo, setTodo] = useState('');
+
+
+
+  function getDayName(date = new Date(), locale = 'en-US') {
+    return date.toLocaleDateString(locale, { weekday: 'long' });
+  } //to find current day
+
+
+
+  const handleCheckboxChange = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, status: !todo.status } : todo
+      )
+    );
+  };
+
+  function deleteTodo(id) {
+    // Find the index of the todo item with the specified id
+    const index = toDos.findIndex((todo) => todo.id === id);
+
+    if (index !== -1) {
+      // Create a new array without the todo item to be deleted
+      const updatedTodos = [...toDos.slice(0, index), ...toDos.slice(index + 1)];
+
+      // Update the state with the new array
+      setTodos(updatedTodos);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {getDayName()} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(event) => setTodo(event.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>
+            setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])
+          }
+          className="fas fa-plus"
+        ></i>
+      </div>
+
+      <div className="todos">
+        {toDos.map((obj) => (
+          <div className="todo" key={obj.id}>
+
+
+            <div className="left">
+              <input
+                onChange={() => handleCheckboxChange(obj.id)}
+                checked={obj.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p style={{ textDecoration: obj.status ? 'line-through' : 'none' }}>
+                {obj.text}
+              </p>
+            </div>
+
+
+
+
+            <div className="right">
+              <i onClick={() => deleteTodo(obj.id)} className="fas fa-times"></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
 export default App;
